c0f515ec1e19cccdd1c625f6eb4d36963fdb3bca
BERCHIL Idriss <16048625+Jonahgay@users.noreply.github.com>
2022-11-10 08:54:03 +0100

    Cours sur les (smart) pointers

--- stuff.cpp
+++ stuff.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 //auto a(int a, int b = 5)-> int;
+// stack heap => pointer(stack)
+//instead of malloc use new Type
+/*
+    int *p new int; // heap allocation, p is located in the stack
+    *p = 5; // 5 is in the heap
+    delete p; // free 5 from memory !!p is still in the stack!!
+*/
 int main()
 {
     
