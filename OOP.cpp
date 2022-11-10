2d3d03198592ec06bb3c2ae980bee62aa9129d65
BERCHIL Idriss <16048625+Jonahgay@users.noreply.github.com>
2022-11-10 09:56:39 +0100

    Cours POO

---
+++ OOP.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+// UML + public -private 
+// attrib methods
+
+/*
+    class Compte
+    {
+        //par defaut private
+        string nom;
+        double solde;
+        int numero;
+        public:
+            ---
+            ---
+    }
+*/
+int main()
+{
+    
+    return 0;
+}
